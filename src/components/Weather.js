1f1ad639853ee9141b7b9b20f9df016ed2d581ce
Hemant Sirsat <hemantsirsat40@gmail.com>
2020-12-13 14:57:41 +0530

    Display of weather done. Icon Remaining

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import useWeatherDetails from '../hooks/useWeatherDetails';
+import { Feather } from '@expo/vector-icons';
 
 const Weather = () => {   
-    const [weatherInfo, errorMessageWeather] = useWeatherDetails();
-
+    const [FetchWeather, Temperature, TemperatureMax, TemperatureMin, City, Description, Wind, IconID, errorMessage] = useWeatherDetails();
+    
+    useEffect(()=>{
+        FetchWeather();
+    },[]);
+    
     return(
         <View style={styles.viewStyle}>
-            
+            <View style={styles.firstLineStyle}>
+                <Text style={styles.cityStyle}>{Temperature}&deg; in {City}</Text>
+                <Text style={styles.minmaxtextStyle}>{TemperatureMax}&deg;/{TemperatureMin}&deg;</Text>
+            </View>
+            <View style={styles.secondLineStyle}>
+                <Text style={styles.descriptionStyle}>{Description}</Text>
+        
+                <Text style={styles.windStyle}><Feather name="wind" size={18} color="white" /> {Wind} m/s</Text>
+            </View>
         </View>
     );
 };
@@ -20,6 +33,30 @@ const styles = StyleSheet.create({
         marginHorizontal:18,
         marginVertical:7,
         borderRadius:10,
+        padding:20
+    },
+    firstLineStyle:{
+        flexDirection:'row'
+    },
+    secondLineStyle:{
+        flexDirection:'row',
+        paddingTop:10,
+    },
+    cityStyle:{
+        color:'#fff',
+        paddingLeft:50
+    },  
+    minmaxtextStyle:{
+        marginLeft:30,
+        color:'#fff'
+    },
+    windStyle:{
+        color:"#fff",
+        marginLeft:97
+    },
+    descriptionStyle:{
+        color:'#fff',
+        paddingLeft:50
     },
     textStyle:{
         color:'#fff',
--- src/hooks/useWeatherDetails.js
+++ src/hooks/useWeatherDetails.js
@@ -3,8 +3,14 @@ import openweather from '../api/openweather';
 import useCurrentLocation from '../hooks/useCurrentLocation';
 
 export default () =>{
-    const [weatherInfo, setweatherInfo] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
+    const [Temperature, setTemperature] = useState('');
+    const [TemperatureMax, setTemperatureMax] = useState('');
+    const [TemperatureMin, setTemperatureMin] = useState('');
+    const [City, setCity] = useState('');
+    const [Description, setDescription] = useState('');
+    const [Wind, setWind] = useState('');
+    const [IconID, setIconID] = useState('');
     const [currentLocationLatitude, currentLocationLongitude, errorMessageLocation] = useCurrentLocation();
 
     const FetchWeather = async () => {
@@ -17,15 +23,19 @@ export default () =>{
                     appid:"894782978af57bdfa9df4954dfa1b0a8" 
                 }
             });
-            setweatherInfo(response.data);
+            
+            setTemperature(response.data.main.temp);
+            setTemperatureMax(response.data.main.temp_max);
+            setTemperatureMin(response.data.main.temp_min);
+            setCity(response.data.name);
+            setDescription(response.data.weather[0].main);
+            setWind(response.data.wind.speed);
+            setIconID(response.data.weather[0].icon);
         }
         catch(err){
             setErrorMessage("Something Went Wrong!");
         }
     };
-    useEffect(()=>{
-        FetchWeather();
-    },[])
 
-    return [weatherInfo, errorMessage];
+    return [FetchWeather, Temperature, TemperatureMax, TemperatureMin, City, Description, Wind, IconID, errorMessage];
 }
